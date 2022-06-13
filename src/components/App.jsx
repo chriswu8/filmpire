12fe9c07a4df6ec004a6d32bc7921a612c8c54ec
Chris Wu <zezemint@gmail.com>
2022-06-12 21:16:37 -0700

    set up BrowserRouter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+
+// used to created different pages
+import { Route, Switch } from 'react-router-dom';
+
+// / -> root -> all movies
+// /abc123 -> movie information -> more
 
 const App = () => (
 
-  <div>App</div>
+  <div>
+    <CssBaseline />
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <h1>home</h1>
+        </Route>
+        <Route exact path="/movies">
+          <h1>movies</h1>
+        </Route>
+      </Switch>
+    </main>
+  </div>
 
 );
 
